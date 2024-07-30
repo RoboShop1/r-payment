f49fb999800d184b5ed25e9ce36563e9851ef994
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-30 07:31:17 +0530

    m

--- payment.py
+++ payment.py
@@ -105,7 +105,7 @@ def pay(id):
     # add to order history
     if not anonymous_user:
         try:
-            req = requests.post('http://{user}:8080/order/{id}'.format(user=USER, id=id),
+            req = requests.post('http://{user}:{userPort}/order/{id}'.format(user=USER,userPort=USER_PORT, id=id),
                     data=json.dumps({'orderid': orderid, 'cart': cart}),
                     headers={'Content-Type': 'application/json'})
             app.logger.info('order history returned {}'.format(req.status_code))
